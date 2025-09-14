6fc77a0be924cfb159e14487ca07688938030f86
cleytonorocha <cleytoodeoliveirarocha@gmail.com>
2025-09-14 20:04:54 -0300

    refact: change the envs

--- src/service/ProductService.js
+++ src/service/ProductService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = "http://localhost:8080" + "/products";
+const API_URL = "http://localhost:8080" + "/product";
 
 export const getAllProducts = () => {
     return axios.get(API_URL)
