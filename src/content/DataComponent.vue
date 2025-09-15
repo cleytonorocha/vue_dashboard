4cf497a626636aadd7134cd8e3e4a6fa305b6a78
cleytonorocha <cleytoodeoliveirarocha@gmail.com>
2025-09-14 21:52:29 -0300

    fix: pagination

--- src/content/DataComponent.vue
+++ src/content/DataComponent.vue
@@ -1,76 +1,41 @@
 <template>
   <div id="data" class="container-fluid p-4">
-    <el-table
-      :data="
-        products.filter(
-          (product) =>
-            !search || product.name.toLowerCase().includes(search.toLowerCase())
-        )
-      "
-      stripe
-      style="width: 100%; font-size: 16px"
-    >
+    <el-table :data="products.filter(
+      (product) =>
+        !search || product.name.toLowerCase().includes(search.toLowerCase())
+    )
+      " stripe style="width: 100%; font-size: 16px">
       <el-table-column prop="id" label="ID" width="100"></el-table-column>
-      <el-table-column
-        prop="name"
-        label="Name"
-        min-width="200"
-      ></el-table-column>
-      <el-table-column
-        prop="category"
-        label="Category"
-        min-width="150"
-      ></el-table-column>
+      <el-table-column prop="name" label="Name" min-width="200"></el-table-column>
+      <el-table-column prop="category" label="Category" min-width="150"></el-table-column>
       <el-table-column prop="stock" label="Stock" width="100"></el-table-column>
-      <el-table-column
-        prop="rating"
-        label="Rating"
-        width="100"
-      ></el-table-column>
+      <el-table-column prop="rating" label="Rating" width="100"></el-table-column>
       <el-table-column prop="price" label="Price" width="150"></el-table-column>
 
       <el-table-column align="right" width="200">
         <template #header>
-          <el-input
-            v-model="search"
-            size="mini"
-            placeholder="Type to search"
-            clearable
-          />
+          <el-input v-model="search" size="mini" placeholder="Type to search" clearable />
         </template>
 
         <template #default="scope">
-          <el-button
-            size="small"
-            type="primary"
-            @click="editProduct(scope.row)"
-          >
+          <el-button size="small" type="primary" @click="editProduct(scope.row)">
             Edit
           </el-button>
-          <el-button
-            size="small"
-            type="danger"
-            @click="confirmDelete(scope.row)"
-          >
+          <el-button size="small" type="danger" @click="confirmDelete(scope.row)">
             Delete
           </el-button>
         </template>
       </el-table-column>
     </el-table>
 
-    <el-dialog
-      title="Warning"
-      :visible.sync="excludeDialog"
-      width="30%"
-      center
-    >
+    <el-pagination class="text-center my-5" background layout="sizes, prev, pager, next" :total="pagination.total" size :page-sizes="pageSizes"
+      @size-change="handleSizeChange" @current-change="handleCurrentChange" />
+
+    <el-dialog title="Warning" :visible.sync="excludeDialog" width="30%" center>
       <span>Are you sure you want to delete this product?</span>
       <span slot="footer" class="dialog-footer">
         <el-button @click="excludeDialog = false">Cancel</el-button>
-        <el-button
-          type="danger"
-          @click="deleteProduct"
-        >
+        <el-button type="danger" @click="deleteProduct">
           Confirm
         </el-button>
       </span>
@@ -87,39 +52,69 @@ export default {
     return {
       products: [],
       search: "",
+      pagination: {
+        page: 0,
+        linesPerPage: 10,
+        orderBy: 'id',
+        direction: 'ASC',
+      },
+      pageSizes: [10, 50, 100],
       excludeDialog: false,
-      productToDelete: null, 
+      productToDelete: null,
     };
   },
+
   mounted() {
     this.loadProducts();
   },
+
   methods: {
+
     async loadProducts() {
       try {
-        const data = await getAllProducts();
+        const data = await getAllProducts(this.pagination);
         this.products = data.content;
+        this.pagination = {
+          ...this.pagination,
+          page: data.pageable.pageNumber,
+          perPage: data.size,
+          total: data.totalElements,
+          totalPages: data.totalPages
+        }
       } catch (error) {
         console.error("Error loading products:", error);
       }
     },
+
     editProduct(product) {
       console.log("Edit product:", product);
     },
+
     confirmDelete(product) {
-      this.productToDelete = product; 
+      this.productToDelete = product;
       this.excludeDialog = true;
     },
+
     deleteProduct() {
       if (this.productToDelete) {
         this.products = this.products.filter(
           (product) => product.id !== this.productToDelete.id
         );
         deleteProductService(this.productToDelete.id);
-        this.productToDelete = null; 
-        this.excludeDialog = false; 
+        this.productToDelete = null;
+        this.excludeDialog = false
       }
     },
+
+    handleSizeChange(val) {
+      this.pagination.linesPerPage = val
+      this.loadProducts();
+    },
+
+    handleCurrentChange(val) {
+      this.pagination.page = val - 1
+      this.loadProducts();
+    }
   },
 };
 </script>
--- src/service/ProductService.js
+++ src/service/ProductService.js
@@ -1,48 +1,62 @@
 import axios from 'axios';
 
-const API_URL = "http://localhost:8080" + "/product";
+const API_URL = 'http://localhost:8080' + '/product';
 
-export const getAllProducts = () => {
-    return axios.get(API_URL)
-        .then(response => response.data)
-        .catch(error => {
-            console.error('There was an error fetching the products!', error);
-            throw error;
-        });
+export const getAllProducts = (pagination) => {
+  return axios
+    .get(API_URL, { params: { ...pagination } })
+    .then((response) => response.data)
+    .catch((error) => {
+      console.error('There was an error fetching the products!', error);
+      throw error;
+    });
 };
 
 export const getProductById = (productId) => {
-    return axios.get(`${API_URL}/${productId}`)
-        .then(response => response.data)
-        .catch(error => {
-            console.error(`There was an error fetching the product with ID ${productId}!`, error);
-            throw error;
-        });
+  return axios
+    .get(`${API_URL}/${productId}`)
+    .then((response) => response.data)
+    .catch((error) => {
+      console.error(
+        `There was an error fetching the product with ID ${productId}!`,
+        error
+      );
+      throw error;
+    });
 };
 
 export const createProduct = (productData) => {
-    return axios.post(API_URL, productData)
-        .then(response => response.data)
-        .catch(error => {
-            console.error('There was an error creating the product!', error);
-            throw error;
-        });
+  return axios
+    .post(API_URL, productData)
+    .then((response) => response.data)
+    .catch((error) => {
+      console.error('There was an error creating the product!', error);
+      throw error;
+    });
 };
 
 export const updateProduct = (productId, productData) => {
-    return axios.put(`${API_URL}/${productId}`, productData)
-        .then(response => response.data)
-        .catch(error => {
-            console.error(`There was an error updating the product with ID ${productId}!`, error);
-            throw error;
-        });
+  return axios
+    .put(`${API_URL}/${productId}`, productData)
+    .then((response) => response.data)
+    .catch((error) => {
+      console.error(
+        `There was an error updating the product with ID ${productId}!`,
+        error
+      );
+      throw error;
+    });
 };
 
 export const deleteProduct = (productId) => {
-    return axios.delete(`${API_URL}/${productId}`)
-        .then(response => response.data)
-        .catch(error => {
-            console.error(`There was an error deleting the product with ID ${productId}!`, error);
-            throw error;
-        });
+  return axios
+    .delete(`${API_URL}/${productId}`)
+    .then((response) => response.data)
+    .catch((error) => {
+      console.error(
+        `There was an error deleting the product with ID ${productId}!`,
+        error
+      );
+      throw error;
+    });
 };
