c75a448a909035547e666f4088a4b121d6603b57
cleytonorocha <cleytoodeoliveirarocha@gmail.com>
2025-09-21 19:04:32 -0300

    feat: dialog and report content

--- src/content/ReportContent.vue
+++ src/content/ReportContent.vue
@@ -1,58 +1,131 @@
 <template>
-    <div id="report">
-        <div class="row d-flex justify-content-around py-4">
-            <card-template onclick="openDialog(1)">
-                <template v-slot:card-title>
-                    <h2>List Produtcs</h2>
-                </template>
+  <div id="report">
+    <div class="row d-flex justify-content-around py-4">
+      <card-template
+        v-for="(item, index) in defaults"
+        :key="index"
+        @click.native="openDialog(item, item.configIndex)"
+      >
+        <template v-slot:card-title>
+          <h2>{{ item.nameHeader }}</h2>
+        </template>
 
-                <template v-slot:card-content>
-                    <img src="" alt="">
-                </template>
-            </card-template>
-
-            <card-template>
-                <template v-slot:card-title>
-                    <h2>Coluna</h2>
-                </template>
-
-                <template v-slot:card-content>
-                    <p>Este é o conteúdo do card, passado dinamicamente!</p>
-                </template>
-            </card-template>
-
-            <card-template>
-                <template v-slot:card-title>
-                    <h2>Coluna</h2>
-                </template>
-                <template v-slot:card-content>
-                    <p>Este é o conteúdo do card, passado dinamicamente!</p>
-                </template>
-            </card-template>
-
-            <card-template>
-                <template v-slot:card-title>
-                    <h2>Coluna</h2>
-                </template>
-
-                <template v-slot:card-content>
-                    <p>Este é o conteúdo do card, passado dinamicamente!</p>
-                </template>
-            </card-template>
-        </div>
+        <template v-slot:card-content>
+          <img src="image source" class="img-fluid rounded-top" alt="" />
+        </template>
+      </card-template>
     </div>
+
+    <el-button
+      v-loading.fullscreen.lock="submitLoading"
+      v-if="false"
+    ></el-button>
+
+    <dialog-template
+      ref="DialogTemplate"
+      @handleDialogSubmit="handleDialogSubmit"
+      @closeDialog="configIndex = null"
+    />
+  </div>
 </template>
+
 <script>
 import CardTemplate from "@/templates/CardTemplate.vue";
+import DialogTemplate from "@/templates/DialogTemplate.vue";
+import { listCategory, listStatus } from "@/service/ProductService";
 
 export default {
-    name: "ReportContent",
-    components: {
-        CardTemplate,
+  name: "ReportContent",
+  components: { CardTemplate, DialogTemplate },
+  mounted: async function () {
+    this.categoryList = await listCategory().then((t) => t.data);
+    this.statusList = await listStatus().then((t) => t.data);
+  },
+  data() {
+    return {
+      configIndex: null,
+      categoryList: [],
+      statusList: [],
+      config: [
+        {
+          name: "ProductsReport",
+          fields: [
+            {
+              name: "name",
+              label: "Name",
+              type: "text",
+              placeholder: "Enter the name",
+            },
+            {
+              name: "productCategory",
+              label: "Category",
+              type: "select",
+              categoryList: [],
+            },
+            {
+              name: "productStatus",
+              label: "Status",
+              type: "select",
+              options: [],
+            },
+            {
+              name: "minPrice",
+              label: "Minimum Price",
+              type: "number",
+              placeholder: "Enter minimum price",
+            },
+            {
+              name: "maxPrice",
+              label: "Maximum Price",
+              type: "number",
+              placeholder: "Enter maximum price",
+            },
+          ],
+        },
+      ],
+
+      defaults: [
+        {
+          nameHeader: "Products Report",
+          name: "Product 1",
+          productCategory: "",
+          productStatus: "",
+          minPrice: 10,
+          maxPrice: 50,
+          configIndex: 0,
+        },
+      ],
+    };
+  },
+  methods: {
+    openDialog(itemData, configIndex) {
+      const fields = this.config[configIndex].fields.map((f) => {
+        switch (f.name) {
+          case "productCategory":
+            return { ...f, options: this.categoryList };
+          case "productStatus":
+            return { ...f, options: this.statusList };
+          default:
+            return f;
+        }
+      });
+
+      this.configIndex = configIndex;
+
+      this.$refs.DialogTemplate.openDialog({
+        data: { ...itemData },
+        fields,
+        config: { title: this.config[configIndex].name, width: "80%" },
+      });
     },
-    methods: {
-        
+    async handleDialogSubmit(formData) {
+      this.$refs.DialogTemplate.submitLoading = true;
+      switch (this.closeDialog) {
+        case 0:
+          break
+      }
+      this.$refs.DialogTemplate.submitLoading = false;
     },
+  },
 };
 </script>
-<style></style>
---
+++ src/templates/DialogTemplate.vue
@@ -0,0 +1,91 @@
+<template>
+  <el-dialog
+    :title="configDialog.title || 'Dialog'"
+    :width="configDialog.width || '30%'"
+    :visible.sync="visible"
+    :before-close="closeDialog"
+    append-to-body
+    v-loading.fullscreen.lock="submitLoading"
+  >
+    <el-form :model="formData" label-position="top" class="row g-3">
+      <el-form-item
+        v-for="(field, index) in fields"
+        :key="index"
+        :label="field.label || field.name"
+        :prop="field.name"
+        :class="field.class"
+        class="col-12 col-lg-6"
+      >
+        <el-input
+          v-if="field.type === 'text'"
+          v-model="formData[field.name]"
+          :placeholder="field.placeholder || ''"
+          class="w-100"
+        />
+
+        <el-input
+          v-else-if="field.type === 'number'"
+          v-model.number="formData[field.name]"
+          type="number"
+          :placeholder="field.placeholder || ''"
+          class="w-100"
+        />
+
+        <el-select
+          v-else-if="field.type === 'select'"
+          v-model="formData[field.name]"
+          :placeholder="field.placeholder || 'Select'"
+          class="w-100"
+        >
+          <el-option
+            v-for="option in field.options"
+            :key="option.key"
+            :label="option.description"
+            :value="option.key"
+            class="w-100"
+          />
+        </el-select>
+      </el-form-item>
+
+      <div class="dialog-footer" style="text-align: right; margin-top: 1rem">
+        <el-button @click="closeDialog">Cancel</el-button>
+        <el-button
+          type="primary"
+          @click="handleSubmit"
+          v-loading.fullscreen.lock="submitLoading"
+          >Submit</el-button
+        >
+      </div>
+    </el-form>
+  </el-dialog>
+</template>
+
+<script>
+export default {
+  name: "DialogTemplate",
+  data() {
+    return {
+      visible: false,
+      formData: {},
+      fields: [],
+      configDialog: {},
+      submitLoading: false,
+    };
+  },
+  methods: {
+    openDialog({ data = {}, fields = [], config = {} }) {
+      this.formData = { ...data };
+      this.fields = fields;
+      this.configDialog = config;
+      this.visible = true;
+    },
+    closeDialog() {
+      this.visible = false;
+      this.$emit("closeDialog");
+    },
+    async handleSubmit() {
+      this.$emit("handleDialogSubmit", { ...this.formData });
+    },
+  },
+};
+</script>
